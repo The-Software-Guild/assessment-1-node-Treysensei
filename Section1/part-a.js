6780e44007ef78d5d7e13c53e5f2c388904c2a43
Treysensei <Bmontria@gmail.com>
2022-02-06 15:09:52 -0600

    Section-one completed

--- Section1/part-a.js
+++ Section1/part-a.js
@@ -1,3 +1,13 @@
-const random5 = () => Math.round(Math.random()*7)
+//const random5 = () => Math.round(Math.random()*5)
 
-console.log(random5());
+
+//console.log(random5());
+
+const random5 = () => {
+    return 1 + Math.random() * 4;
+  }
+
+  console.log(Math.round(random5()*1.4));
+
+
+  
